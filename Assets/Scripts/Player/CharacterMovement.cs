ba6b8bad63e82128dcbbdde52a4693be1fc87336
Mithat <mithatsirmali@gmail.com>
2025-12-15 18:06:22 +0300

    Prepare for player

--- Assets/Scripts/Player/CharacterMovement.cs
+++ Assets/Scripts/Player/CharacterMovement.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
 
 public class CharacterMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Component References")]
+    private Rigidbody2D rb;
+    private Animator animator;
+    private AudioSource audioSource;
+
+    [Header("Player Settings")]
+    public float moveSpeed = 15f;
+    private float health = 300f;
+    public TextMeshProUGUI healthBar;
+
+
+
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     void Update()
     {
 
+    }
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 move = new Vector2(x, y).normalized;
+        rb.linearVelocity = new Vector2(move.x * moveSpeed, move.y * moveSpeed);
+    }
+    public void GetDamage(float damage)
+    {
+        health -= damage;
+        healthBar.text = health.ToString();
     }
 }
